ebe9bdf71cf9f43e22565b2b06d1ef2c83cae273
Rob <vbtechnieken@gmail.com>
2023-11-08 23:21:43 +0100

    update

--- sources/search_and_fill.c
+++ sources/search_and_fill.c
@@ -3,14 +3,15 @@
 #include "ft_filereader.h"
 #include "search_and_fill.h"
 #include "ft_valid.h"
-//#include <unistd.h>
+#include <unistd.h>
 #include <stdlib.h>
 #include <fcntl.h>
 #include <stdio.h>
 
 int search_and_fill(char *file, int filesize, board_settings *settings)
 {
-    fill_board(file,filesize,settings);
+    if(!fill_board(file,filesize,settings))
+        return (-1);
     find_biggest_square(settings);
     print_grid(settings);
     return (1);
@@ -38,7 +39,7 @@ int	fill_board(char *file, int filesize, board_settings *settings)
 			if (c == '\n')
 				c = file[horizontal + vertical + skipparams + 1];
 			if (!(c == settings->empty || c == settings->obstacle))
-				return (1);
+				return (-1);
 			line[horizontal] = c;
 			horizontal++;
 		}
@@ -47,26 +48,8 @@ int	fill_board(char *file, int filesize, board_settings *settings)
 		horizontal = 0;
 		vertical++;
 	}
-	return (0);
+	return (1);
 }
-
-
-
-int can_place_square(int x, int y, int size,board_settings *board) {
-    if (x + size > board->rows || y + size > board->line_lenght) {
-        return 0;
-    }
-
-    for (int i = x; i < x + size; i++) {
-        for (int j = y; j < y + size; j++) {
-            if (board->board[i][j] == board->obstacle) {
-                return 0;
-            }
-        }
-    }
-    return 1;
-}
-
 
 void find_biggest_square(board_settings *settings) {
     int max_size = 0;
@@ -97,39 +80,21 @@ void find_biggest_square(board_settings *settings) {
     }
 }
 
+int can_place_square(int x, int y, int size,board_settings *board) {
+    if (x + size > board->rows || y + size > board->line_lenght) {
+        return 0;
+    }
 
-
-
-
-
-
-/*
-void find_biggest_square(board_settings *board) {
-    int max_size = 0;
-    int max_x = 0;
-    int max_y = 0;
-
-    for (int x = 0; x < board->rows; x++) {
-        for (int y = 0; y < board->line_lenght; y++) {
-            int size = 1;
-            while (can_place_square(x, y, size, board)) {
-                size++;
-            }
-
-            if (size > max_size) {
-                max_size = size;
-                max_x = x;
-                max_y = y;
+    for (int i = x; i < x + size; i++) {
+        for (int j = y; j < y + size; j++) {
+            if (board->board[i][j] == board->obstacle) {
+                return 0;
             }
         }
     }
+    return 1;
+}
 
-    for (int i = max_x; i < max_x + max_size; i++) {
-        for (int j = max_y; j < max_y + max_size; j++) {
-            board->board[i][j] = board->fill;
-        }
-    }
-}*/
 
 void print_grid(board_settings *board) {
     for (int i = 0; i < board->rows; i++) {
