b9b6999c3035ba8d445cf6178c82e905c558041f
Rob <vbtechnieken@gmail.com>
2023-11-08 22:08:23 +0100

    update

---
+++ sources/main.c
@@ -0,0 +1,98 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rvan-bae <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/07 10:47:31 by rvan-bae          #+#    #+#             */
+/*   Updated: 2023/11/08 15:47:11 by rperez-t         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_h.h"
+#include "ft_filehandeling.h"
+#include "search_and_fill.h"
+#include "ft_filereader.h"
+//#include "ft_board.h"
+#include "ft_valid.h"
+//#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <stdio.h>
+
+static board_settings *settings;
+
+void init()
+{
+	settings = malloc(sizeof(board_settings));
+}
+
+void init_board(board_settings *settings)
+{	
+	int i;
+
+	i =0;
+
+	settings->board = malloc(sizeof(char *) * (settings->rows + 1));
+	while(i < settings->rows)
+	{
+		settings->board[i] = malloc (sizeof(char) * (settings->line_lenght + 1));
+		i++;
+	}
+	settings->board[i] = 0; 
+}
+
+void free_board()
+{
+	int i;
+	
+	i = 0;
+	while(i <= settings->rows)
+	{
+		free(settings->board[i]);
+		i++;
+	}
+	free(settings->board);
+	free(settings);
+}
+
+int	main(int argc, char **argv)
+{
+	int				index;
+	char			*file;
+	//char			*line;
+	
+	init();	
+
+	index = 1;
+	if (argc < 2)
+	{
+		write(2, "map error\n", 10);
+		return (0);
+	}
+	file = ft_return_file(argv[1]);
+	int fs = file_size(argv[1]);
+	printf("%s\n", file);
+	//line = readline(file,1);
+	//printf("%i\n",fs);
+	//int ls = get_line_size(file,fs,0);
+	//printf("%i\n",ls);
+	while (index < argc)
+	{
+		if (!check_map_valid(argv[index], settings))
+		{
+			write(2, "map error\n", 10);
+			return (0);	
+		}
+		index++;
+	}
+	init_board(settings);
+	search_and_fill(file,fs,settings);
+	//printf("\n\n");
+	//for(int i = 0;i<settings->rows;i++)
+		//printf("%s\n", settings->board[i]);
+	free(file);
+	free_board();
+	return (0);
+}
---
+++ sources/search_and_fill.c
@@ -0,0 +1,104 @@
+#include "ft_h.h"
+#include "ft_filehandeling.h"
+#include "ft_filereader.h"
+#include "search_and_fill.h"
+#include "ft_valid.h"
+//#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <stdio.h>
+
+int search_and_fill(char *file, int filesize, board_settings *settings)
+{
+    fill_board(file,filesize,settings);
+    find_biggest_square(settings);
+    print_grid(settings);
+}
+
+
+
+int	fill_board(char *file, int filesize, board_settings *settings)
+{
+	int		vertical;
+	int		horizontal;
+	int		skipparams;
+	char	*line;
+	char	c;
+
+	vertical = 0;
+	horizontal = 0;
+	skipparams = get_line_size (file, filesize, 0);
+	while (vertical < settings->rows)
+	{
+		line = settings->board[vertical];
+		while (horizontal < settings->line_lenght)
+		{
+			c = file[horizontal + vertical + skipparams];
+			if (c == '\n')
+				c = file[horizontal + vertical + skipparams + 1];
+			if (!(c == settings->empty || c == settings->obstacle))
+				return (1);
+			line[horizontal] = c;
+			horizontal++;
+		}
+		line[horizontal] = 0;
+		skipparams += horizontal;
+		horizontal = 0;
+		vertical++;
+	}
+	return (0);
+}
+
+
+
+int can_place_square(int x, int y, int size,board_settings *board) {
+    if (x + size > board->rows) {
+        return 0;
+    }
+
+    for (int i = x; i < x + size; i++) {
+        if (strlen(board->board[i]) - y < size) {
+            return 0;
+        }
+
+        for (int j = y; j < y + size; j++) {
+            if (board->board[i][j] == board->obstacle) {
+                return 0;
+            }
+        }
+    }
+    return 1;
+}
+
+void find_biggest_square(board_settings *board) {
+    int max_size = 0;
+    int max_x = 0;
+    int max_y = 0;
+
+    for (int x = 0; x < board->rows; x++) {
+        for (int y = 0; y < strlen(board->board[x]); y++) {
+            int size = 1;
+            while (can_place_square(x, y, size, board)) {
+                size++;
+            }
+
+            if (size - 1 > max_size) {
+                max_size = size - 1;
+                max_x = x;
+                max_y = y;
+            }
+        }
+    }
+
+    for (int i = max_x; i < max_x + max_size; i++) {
+        for (int j = max_y; j < max_y + max_size; j++) {
+            board->board[i][j] = board->fill;
+        }
+    }
+}
+
+void print_grid(board_settings *board) {
+    for (int i = 0; i < board->rows; i++) {
+        printf("%s\n", board->board[i]);
+    }
+}
